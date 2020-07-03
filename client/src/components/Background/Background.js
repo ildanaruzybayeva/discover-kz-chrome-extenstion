dd3b06b4c80862b05d85edfc675eb59a92e4bc33
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-03 19:06:34 +0000

    Added search params

--- client/src/components/Background/Background.js
+++ client/src/components/Background/Background.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
+import Unsplash, { toJson } from "unsplash-js";
+import "./Background.css";
 
-const URL =
-  "https://api.unsplash.com/search/photos/?client_id=1911bed2e67cf6900e231737eb1ebabaca3168bdb5f7908087f046ff1c820fd1&query=nature";
+const unsplash = new Unsplash({
+  accessKey: "1911bed2e67cf6900e231737eb1ebabaca3168bdb5f7908087f046ff1c820fd1",
+  secret: "3ecb1c1711fdc64c6711be8dfcd0ab2705bf5d1c7242e8ebc8c5665791b678a6"
+});
 
-const randIdx = Math.floor(Math.random() * 10);
+const randIdx = Math.floor(Math.random() * 15);
 
 function Background() {
   const [image, setImage] = useState("");
   useEffect(() => {
-    fetch(URL)
-      .then(response => response.json())
-      .then(data => setImage(data.results[randIdx].urls.regular));
+    unsplash.search
+      .photos("almaty", 1, 15, { orientation: "landscape", order_by: "latest" })
+      .then(toJson)
+      .then(data => {
+        setImage(data.results[randIdx].urls.raw);
+      });
   }, []);
   return (
-    <div>
+    <div className="background">
       <img src={image} alt="kz" />
     </div>
   );
