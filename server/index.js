27357158d82c9548868d06d49158a95a658ace84
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-05 21:22:54 +0000

    Added weather api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const cors = require("cors");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(cors());
+
+app.use("/", require("./routes/weather"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ server/routes/weather.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+const API_KEY = "f33a484cf794d08d0148764789aaba32";
+const city = "Almaty";
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+router.get("/weather", async (req, res) => {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+module.exports = router;
