ccfe4329a73abca94713cb7d92a9f2e8f6b8ed75
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-09 16:51:44 +0000

    Added currency KZT USD EUR api

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(cors());
 
 app.use("/", require("./routes/weather"));
 app.use("/", require("./routes/unsplash"));
+app.use("/", require("./routes/currency"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ server/routes/currency.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+const apiKey = "707f7a9d41d184115b53";
+const URL = `https://free.currconv.com/api/v7/convert?q=USD_KZT,EUR_KZT&compact=ultra&apiKey=${apiKey}`;
+
+router.get("/curency", async (req, res) => {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+module.exports = router;
