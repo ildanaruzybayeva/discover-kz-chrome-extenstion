bae70027c9b663b0b89b269e3fcd2d8cb4b5aa0a
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-09 16:52:39 +0000

    Currency API

--- server/routes/currency.js
+++ server/routes/currency.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 const apiKey = "707f7a9d41d184115b53";
 const URL = `https://free.currconv.com/api/v7/convert?q=USD_KZT,EUR_KZT&compact=ultra&apiKey=${apiKey}`;
 
-router.get("/curency", async (req, res) => {
+router.get("/currency", async (req, res) => {
   try {
     const response = await fetch(URL);
     const data = await response.json();
