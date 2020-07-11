8177836de002636724b9e5262d0d818af17d8950
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-11 18:56:59 +0000

    Greeting

---
+++ client/src/components/Greeting/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { almatyTime } from "../../utils/helpers/helpers";
+
+console.log(almatyTime);
+
+export default function Greeting() {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Salem, Ildana</h1>
+    </div>
+  );
+}
