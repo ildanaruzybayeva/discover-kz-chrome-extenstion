dd033e2fdb6d4893fc7493e1dfa11c3fbc7f1a63
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-11 18:56:47 +0000

    Added styles

---
+++ client/src/components/Currency/Currency.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Currency.css";
+
+export default function Currency(props) {
+  return (
+    <div>
+      <p>USD-KZT: 123456</p>
+      <p>EUR-KZT: 123456</p>
+    </div>
+  );
+}
