09e9205a57b9539f956b7b1e2c3b985203f626d4
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-12 14:49:15 +0000

    Reduced code

--- client/src/components/Background/Background.js
+++ client/src/components/Background/Background.js
@@ -2,43 +2,48 @@ import React, { useState, useEffect } from "react";
 import "./Background.css";
 import Weather from "../Weather/Weather";
 import Image from "../Image/Image";
-import { almatyTime, randNum } from "../../utils/helpers/helpers";
+import { randNum } from "../../utils/helpers/helpers";
+import Greeting from "../Greeting/Greeting";
+import Currency from "../Currency/Currency";
 
-const URL_PHOTOS =
-  "https://8000-a334ccf2-365c-4afc-8e65-38540926ff08.ws-eu01.gitpod.io/photos";
-const URL_WEATHER =
-  "https://8000-a334ccf2-365c-4afc-8e65-38540926ff08.ws-eu01.gitpod.io/weather";
+const URL =
+  "https://8000-a1ea5764-1c9c-4e01-8116-919f8c363ece.ws-eu01.gitpod.io";
 
 function Background() {
   const [image, setImage] = useState("");
-  const [descriton, setDescription] = useState("");
   const [author, setAuthor] = useState("");
   const [city, setCity] = useState("");
   const [temperature, setTemperature] = useState(0);
-  const [country, setCountry] = useState("");
   const [icon, seticon] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState("");
+  const [currency, setCurrency] = useState({});
 
   const getWeather = () => {
-    const data = fetch(URL_WEATHER)
+    const data = fetch(`${URL}/weather`)
       .then(res => res.json())
       .then(data => {
         setCity(data.name);
         setTemperature(data.main.temp);
-        setCountry(data.sys.country);
         seticon(data.weather[0].icon);
-        setWeatherInfo(data.weather[0].description);
       });
     return data;
   };
 
   const getPhoto = () => {
-    const data = fetch(URL_PHOTOS)
+    const data = fetch(`${URL}/photos`)
       .then(res => res.json())
       .then(data => {
-        setImage(data.results[randNum].urls.regular);
-        setDescription(data.results[randNum].alt_description);
-        setAuthor(data.results[randNum].user.username);
+        setImage(data[randNum].urls.raw);
+        setAuthor(data[randNum].user.username);
+      });
+    return data;
+  };
+
+  const getCurrency = () => {
+    const data = fetch(`${URL}/currency`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setCurrency(data);
       });
     return data;
   };
@@ -46,19 +51,15 @@ function Background() {
   useEffect(() => {
     getWeather();
     getPhoto();
+    getCurrency();
   }, []);
 
   return (
     <div className="container">
-      <Image descriton={descriton} author={author} image={image} />
-      <Weather
-        temperature={temperature}
-        country={country}
-        city={city}
-        icon={icon}
-        weatherInfo={weatherInfo}
-      />
-      <div className="timeAlmaty">{almatyTime}</div>
+      <Image author={author} image={image} />
+      <Weather temperature={temperature} city={city} icon={icon} />
+      <Greeting />
+      <Currency currency={currency} />
     </div>
   );
 }
