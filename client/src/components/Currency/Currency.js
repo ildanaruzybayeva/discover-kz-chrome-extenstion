07bac6a0f13084393ff858e5e82d5def40711cee
sandraols <sandra.helene.olsson@gmail.com>
2020-07-15 15:03:56 +0000

    Added styling on text elements

--- client/src/components/Currency/Currency.js
+++ client/src/components/Currency/Currency.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Currency.css";
 
 export default function Currency(props) {
-  return (
-    <div>
-      <p>USD: {Math.trunc(props.currency.USD_KZT)} ₸</p>
-      <p>EUR: {Math.trunc(props.currency.EUR_KZT)} ₸</p>
-    </div>
-  );
+    return (
+        <div>
+            <p className="paragraph">USD: {Math.trunc(props.currency.USD_KZT)} ₸</p>
+            <p className="paragraph">EUR: {Math.trunc(props.currency.EUR_KZT)} ₸</p>
+        </div>
+    );
 }
