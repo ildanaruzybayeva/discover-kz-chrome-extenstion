b7fed627c902c85a211f65629140e882867af3da
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-23 17:17:32 +0000

    Added current time using react-live-clock

--- client/src/components/Time/Time.js
+++ client/src/components/Time/Time.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './Time.css';
+import React from "react";
+import "./Time.css";
+import Clock from "react-live-clock";
 
-function Time(props) {
-    return (
-    <h1>{props.time}</h1>
-    )
+function Time() {
+  return <Clock format={"HH:mm:ss"} ticking={true} timezone={"Asia/Almaty"} />;
 }
 
 export default Time;
