1de8c1661303f1d96b84db7e845fe56c83616081
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-24 19:54:39 +0000

    Fetched 5 news from Azzatyq

--- server/routes/news.js
+++ server/routes/news.js
@@ -3,34 +3,56 @@ const router = express.Router();
 const fetch = require("node-fetch");
 const puppeteer = require("puppeteer");
 
-const URL = "http://bbc.com/";
+const URL = "https://rus.azattyq.org/z/2231";
 
 router.get("/news", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    }); //Step 1
-    const page = await browser.newPage(); //Step 2
-    await page.goto(url); //Step 3
+    });
+    const page = await browser.newPage();
+    await page.goto(URL);
 
     const news = await page.evaluate(() => {
-      const topNews = [];
-      const listOfAllNews = Array.from(
-        document.querySelectorAll(".media-list__item")
-      ); //page one
-      for (var i = 0; i < 5; i++) {
-        const title =
-          listOfAllNews[i].childNodes[1].children[1].childNodes[1].outerText;
-        topNews.includes(title) ? i++ : topNews.push(title);
-      }
-      return topNews;
-    }); //Step 4
+      const titleOne = document.querySelector(
+        "#ordinaryItems > li:nth-child(1) > div > a"
+      );
+      const titleTwo = document.querySelector(
+        "#ordinaryItems > li:nth-child(2) > div > a"
+      );
+      const titleThree = document.querySelector(
+        "#ordinaryItems > li:nth-child(3) > div > a"
+      );
+      const titleFour = document.querySelector(
+        "#ordinaryItems > li:nth-child(4) > div > a"
+      );
+      const titleFive = document.querySelector(
+        "#ordinaryItems > li:nth-child(5) > div > a"
+      );
+
+      const getNews = (...domSelectors) => {
+        const arrOfNewsObjects = domSelectors.map(obj => ({
+          title: obj.title,
+          link: obj.href
+        }));
+        return arrOfNewsObjects;
+      };
+
+      const result = getNews(
+        titleOne,
+        titleTwo,
+        titleThree,
+        titleFour,
+        titleFive
+      );
+      return result;
+    });
 
     console.log(news);
 
-    res.send(news); //Step 5
-    browser.close(); // Step 6
+    res.send(news);
+    browser.close();
   } catch (err) {
     console.log(err);
     res.send(err);
