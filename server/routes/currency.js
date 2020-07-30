532a5d671ed52665532543f33cff41e406dd6fd5
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-30 16:35:05 +0000

    Added default currency

--- server/routes/currency.js
+++ server/routes/currency.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
-//const URL = `https://free.currconv.com/api/v7/convert?q=USD_KZT,EUR_KZT&compact=ultra&apiKey=${process.env.currencyAPI}`;
+const URL = `https://free.currconv.com/api/v7/convert?q=USD_KZT,EUR_KZT&compact=ultra&apiKey=${process.env.currencyAPI}`;
+
+const defaultRates = {
+  USD_KZT: 415,
+  EUR_KZT: 480
+};
 
 router.get("/currency", async (req, res) => {
   try {
-    const data = {
-      USD_KZT: 415,
-      EUR_KZT: 480
-    };
+    const response = await fetch(URL);
+    const data = await response.json();
     res.send(data);
   } catch (err) {
     console.log(err);
