81399bfce1cac8f6f4d594db9bfbff46b34c35d0
Ildana <ildana.ruzybayeva@gmail.com>
2020-07-31 16:39:33 +0000

    Redered quotes from db

--- client/src/components/Quote/Quote.js
+++ client/src/components/Quote/Quote.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./Quote.css";
 
 function Quote(props) {
-    return (
-        <div className="Quote">
-            <p className="quote paragraph">{props.quote}</p>
-            <p className="quote author">The author</p>
-        </div>
-    )
+  return (
+    <div className="Quote">
+      <p className="quote paragraph">{props.quotes}</p>
+      <p className="quote author">Абай К.</p>
+    </div>
+  );
 }
 
 export default Quote;
